92db80a89c6b7266ef520ba53219f4dad0dc2240
Alexander Shepetko <a@shepetko.com>
2017-12-13 15:42:29 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -8,18 +8,18 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def _init():
-    """Init wrapper.
-    """
+def plugin_load():
     from plugins import assetman
 
     assetman.register_package(__name__)
+
+
+def plugin_load_console():
+    from plugins import assetman
+
     assetman.t_copy(__name__ + '@ckeditor/**', 'ckeditor')
-    assetman.t_js(__name__ + '@*.js')
+    assetman.t_js(__name__)
 
     assetman.js_module('ckeditor-vendor', __name__ + '@ckeditor/ckeditor.min', True)
     assetman.js_module('ckeditor-jquery', __name__ + '@ckeditor/adapters/jquery', True, ['jquery'])
     assetman.js_module('ckeditor', __name__ + '@ckeditor-loader')
-
-
-_init()
