491be4e07f1ef23feaed1a41bf140b780745b130
Alexander Shepetko <a@shepetko.com>
2017-12-14 15:12:56 +0200

    Bugfix 1.1.2

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 """PytSite CKEditor Plugin
 """
-# Public API
-from . import _widget as widget
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
+
+from pytsite import plugman as _plugman
+
+if _plugman.is_installed(__name__):
+    # Public API
+    from . import _widget as widget
 
 
 def plugin_load():
